d5e5df0a045d39d56b1b3bcbbefbeeeee760d8db
EDUARDO MOISES MARTINS <102444133+EduardoMMartins07@users.noreply.github.com>
2024-04-26 21:17:43 -0300

    Delete src directory

--- src/server.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/movieDB', { useNewUrlParser: true, useUnifiedTopology: true });
-const db = mongoose.connection;
-db.on('error', (error) => console.error(error));
-db.once('open', () => console.log('Connected to database'));
